601b794393c559d89de99553f44a129f5035f22e
Rodrigo Moreno <750694@student.dusd.net>
2025-01-16 15:19:04 -0800

    Finished project
    I finished the two scenes for this project that involve waypoints and graphs.

---
+++ WaypointsMorenoP7/Assets/FollowWaypoints.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowWaypoints : MonoBehaviour
+{
+    Transform goal;
+    float speed = 5.0f;
+    float accuracy = 5.0f;
+    float rotSpeed = 2.0f;
+
+    public GameObject wpManager;
+    GameObject[] wps;
+    GameObject currentNode;
+    int currentWP = 0;
+    Graph g;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        g = wpManager.GetComponent<WPManager>().graph;
+        currentNode = wps[0];
+        Time.timeScale = 5;
+
+        //Invoke("GoToRuin", 2);
+    }
+
+    public void GoToHeli()
+    {
+        g.AStar(currentNode, wps[11]);
+        currentWP = 0;
+    }
+
+    public void GoToRuin()
+    {
+        g.AStar(currentNode, wps[4]);
+        currentWP = 0;
+    }
+
+    public void GoToFactory()
+    {
+        g.AStar(currentNode, wps[7]);
+        currentWP = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (g.pathList.Count == 0 || currentWP == g.pathList.Count)
+            return;
+
+        if (Vector3.Distance(g.pathList[currentWP].getId().transform.position, this.transform.position) < accuracy)
+        {
+            currentNode = g.pathList[currentWP].getId();
+            currentWP++;
+        }
+
+        if (currentWP < g.pathList.Count)
+        {
+            goal = g.pathList[currentWP].getId().transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+    }
+}
